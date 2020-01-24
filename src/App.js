b6054d06503b183f00eacef604107e65dd4bfcf3
rajatmw1999 <rajatis1999@gmail.com>
2020-01-24 21:13:48 +0530

    scroll added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import CardList from './CardList';
 import {robots} from './robots'
 import SearchBox from './SearchBox'
 import './App.css'
-
+import Scroll from './Scroll'
 class App extends Component{
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield : ''
     }
   }
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    return response.json();
+  }).then(users => {
+    this.setState({
+      robots: users
+    })
+  })
+
+}
 
 onSearchChange = (event) => {
   this.setState({searchfield : event.target.value});
@@ -21,13 +32,18 @@ onSearchChange = (event) => {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
+    if(this.state.robots.length === 0)
+     return <h1>Loading</h1>
+     else{
     return(
       <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange}/>
+      <Scroll>
       <CardList robots={filteredRobots} />
+      </Scroll>
       </div>
-    );
+    );}
   }
 }
 
